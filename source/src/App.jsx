3bebea06ceb6d58a2be88fc845f49ddb7be0d418
Kovács Zsolt <smith.zsolt@gmail.com>
2018-11-21 23:25:09 +0100

    google verfication 1

--- source/src/App.jsx
+++ source/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactGA from 'react-ga';
 import './App.css';
 import './front/Front'
@@ -7,6 +7,7 @@ import Header from "./header/Header";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Tweet from "./tweet/Tweet";
 import Tag from "./tag/Tag";
+import {Helmet} from "react-helmet";
 
 class App extends Component {
 
@@ -14,17 +15,22 @@ class App extends Component {
         ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
         ReactGA.pageview(window.location.pathname + window.location.search);
         return (
-            <Router>
-                <div className="w-100 p-3">
-                    <Header/>
-                    <Switch>
-                        <Route path="/" exact component={Front}/>
-                        <Route path="/:slug" exact component={Tweet}/>
-                        <Route path="/tag/:slug" exact component={Tag}/>
+            <Fragment>
+                <Helmet>
+                    <meta name="google-site-verification" content={process.env.REACT_APP_GOOGLE_SITE_VERIFICATION} />
+                </Helmet>
+                <Router>
+                    <div className="w-100 p-3">
+                        <Header/>
+                        <Switch>
+                            <Route path="/" exact component={Front}/>
+                            <Route path="/:slug" exact component={Tweet}/>
+                            <Route path="/tag/:slug" exact component={Tag}/>
 
-                    </Switch>
-                </div>
-            </Router>
+                        </Switch>
+                    </div>
+                </Router>
+            </Fragment>
         )
     }
 }
